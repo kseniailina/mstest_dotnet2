c2800f9b157753d9254f2d012f5eeaac8de8c5f6
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-07-11 12:53:06 +0200

    add output to log from tests

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -1,49 +1,62 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Prime.Services;
 
 namespace Prime.UnitTests.Services
 {
-	[TestClass]
-	public class PrimeService_IsPrimeShould
-	{
-		private readonly PrimeService _primeService;
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
 
-		public PrimeService_IsPrimeShould()
-		{
-			_primeService = new PrimeService();
-		}
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
 
-		[TestMethod]
-		public void ReturnFalseGivenValueOf1()
-		{
-			var result = _primeService.IsPrime(1);
+        [TestMethod]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+            
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
 
-			Assert.IsFalse(result, $"1 should not be prime");
-		}
-		
-		[TestMethod]
-		[Ignore]
-		public void IgnoredTest()
-		{
-			var result = _primeService.IsPrime(1);
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
 
-			Assert.IsFalse(result, $"1 should not be prime");
-		}
-		
-		[TestMethod]
-		public void ReturnFalseGivenValueOf3()
-		{
-			var result = _primeService.IsPrime(3);
+        [TestMethod]
+        [Ignore]
+        public void IgnoredTest()
+        {
+            var result = _primeService.IsPrime(1);
+            
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
 
-			Assert.IsFalse(result, $"1 should not be prime");
-		}
-		
-		[TestMethod]
-		public void ReturnFalseGivenValueOf2()
-		{
-			var result = _primeService.IsPrime(2);
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
 
-			Assert.IsFalse(result, $"1 should not be prime");
-		}
-	}
+        [TestMethod]
+        public void ReturnFalseGivenValueOf3()
+        {
+            var result = _primeService.IsPrime(3);
+
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
+
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
+
+        [TestMethod]
+        public void ReturnFalseGivenValueOf2()
+        {
+            var result = _primeService.IsPrime(2);
+
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
+            
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
+    }
 }
