cf2f4bee1a638b6835d41c48614f78739c57688f
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-07-27 13:23:09 +0200

    add xUnit tests

---
+++ PrimeService.Tests/PrimeService_SecondTestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_SecondTestClass
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Decimal.Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Decimal.Add(2, 2));
+        }
+        
+    }
+}
