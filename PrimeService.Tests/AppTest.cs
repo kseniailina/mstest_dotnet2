0a4958dd45e6fe011af6b292845ad9509f69c250
kseniailina <50660900+kseniailina@users.noreply.github.com>
2019-11-05 17:26:51 +0300

    Create AppTest.cs

---
+++ PrimeService.Tests/AppTest.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class AppTest
+    {
+        private readonly PrimeService _primeService;
+        
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+        
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void testAssert21()
+        {            
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
+            TestContext.WriteLine("message from test context");
+
+            var result = _primeService.IsPrime(3);
+
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
+
+     }
+}
